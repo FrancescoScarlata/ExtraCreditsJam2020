8b019c85c4f30e5fe9dc658b0e3393df94316dfb
Francesco Scarlata <francescoscarl93@gmail.com>
2020-08-19 12:39:05 +0200

    Tried to add a local parameter on the test event
    - added a local pitch parameter in the fmod project
    - added a fmod local parameter setter (that doesn't work properly atm)
    - added a button to test the sound effect (it will play the explosion

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/FMODLocalParamaterSetter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for a simple float parameter setter. It can be updated with the different types of parameter usages.
+/// </summary>
+public class FMODLocalParamaterSetter : MonoBehaviour
+{
+
+    private FMOD.Studio.EventInstance instance;
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+    public string parameterName;
+
+    [SerializeField]
+    [Range(-12, 12f)]
+    private float parameterValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+
+    private void Update()
+    {
+        instance.setParameterByName(parameterName, parameterValue);
+    }
+
+    public void SetParameterValueByName()
+    {
+        instance.setParameterByName(parameterName, parameterValue);
+    }
+
+}
---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Tests/Test_SimpleExplosionWithFMOD.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_SimpleExplosionWithFMOD : MonoBehaviour
+{
+    public GameObject fmodGO;
+    public FMODLocalParamaterSetter localSetter;
+    
+    public void DisableAndEnableGO()
+    {
+        fmodGO.SetActive(false);
+        // changes the parameter if it was changed
+        localSetter.SetParameterValueByName();
+        fmodGO.SetActive(true);
+    }
+
+
+}
