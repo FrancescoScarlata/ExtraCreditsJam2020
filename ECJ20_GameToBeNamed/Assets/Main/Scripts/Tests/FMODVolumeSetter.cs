4f65916c8de8cb8fe72a37a341c2773a25ca7ae7
FrancescoS <francescoscarl93@gmail.com>
2020-08-19 19:38:58 +0200

    Added the volume feature with fmod
    In FMOD:
    - moved the Testevent test in a Test folder (and updated the path in Unity)
    - in the mixer, added 2 buses: Master and Test
    - put the Test event test inside the test bus
    In Unity:
    - added 2 sliders for the master and test volumes in the test scene
    - added a volume setter script, that will handle the updates of the slider values
    - moved the ParameterSetter script inside the test folder

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Tests/FMODVolumeSetter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that will change the volume of th sfxs using the buses in fmod
+/// </summary>
+public class FMODVolumeSetter : MonoBehaviour
+{
+
+    // all the buses
+    FMOD.Studio.Bus master;
+    FMOD.Studio.Bus test;
+
+
+    float testVolume;
+    float masterVolume;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // this will assign the bus. after the test it will be a string parameter instead of hard coded
+        test = FMODUnity.RuntimeManager.GetBus("bus:/Master/Test"); 
+        master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+    }
+
+
+    public void UpdateTestVolume(float value)
+    {
+        testVolume = value;
+        test.setVolume(value);
+    }
+    public void UpdateMasterVolume(float value)
+    {
+        masterVolume = value;
+        master.setVolume(value);
+    }
+}
