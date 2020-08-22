af45cf88de849d06e7a18760a32c49d59166274e
FrancescoS <francescoscarl93@gmail.com>
2020-08-22 18:36:22 +0200

    Added a scene to test the layout of the market
    - also added the counter interaction (that will do nothing for now)
    - added a sorting layer for the background: now the sorting layers are Default and Background

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Interactions/CounterInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for the iteraction this will be used by the player to go finish the supermarket phase.
+/// </summary>
+public class CounterInteraction : _Interactions
+{
+    /// <summary>
+    /// Method called to finish the phase
+    /// </summary>
+    public override void Interact()
+    {
+        Debug.Log("End phase!");
+
+        // this will stop the phase, make a fade and then call the next phase [to do]
+        //GameManager.instance.NextPhase();
+    }
+
+
+}
