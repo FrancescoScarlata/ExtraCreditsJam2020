e4e2f877407bf657d269ec05f02bf046ca2d9b5c
Iain <iain.skaggs@gmail.com>
2020-08-22 14:59:01 +0800

    Added scene and scripts for movement and interaction testing
    -Created scene for testing player movement and interactions
    -Added SimpleCameraFollow.cs script for camera movement
    -Added PlayerController.cs for player movement and behaviours
    -Made and added test sprites for a greybox supermarket environment and player

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class that manages Player behaviours, mainly movement. Virtually everything related to the display (sprites et al.) is presently 
+/// for testing purposes and will most certainly be changed.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 20f)]
+    float moveSpeed = 5f;
+
+    SpriteRenderer sprRend;
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprRend = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Receive inputs
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical"));
+
+        if(movement.x < 0)
+        {
+            sprRend.flipX = true;
+        }
+        else if(movement.x > 0)
+        {
+            sprRend.flipX = false;
+        }
+    }
+
+    // Execute movement
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        // Could instead do rb.velocity for a bit of a ramp-up to speed, not sure which feels better?
+    }
+}
---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Player/SimpleCameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A simple class to attach to the scene's main camera that tells it to follow the position of the Player with a small offset, lerp
+///  for a smooth feeling follow effect.
+/// </summary>
+public class SimpleCameraFollow : MonoBehaviour
+{
+    #region Declared Variables
+    public GameObject target;
+    Vector3 offset;
+
+    [SerializeField]
+    [Range(0f, 10f)]
+    float speed = 5.0f;
+
+    bool b;
+    #endregion
+
+    private void LateUpdate()
+    {
+        // Find the Player GameObject if not already assigned via Inspector
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+            return;
+        }
+        else
+        {
+            // Record the position difference between the camera pos and Player pos as the offset. (we probably don't need this, copy+pasted from a previous project)
+            if (!b)
+            {
+                offset = transform.position - target.transform.position;
+                b = true;
+            }
+
+            // Lerp the camera position to the player position w/ offset.
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * speed);
+            return;
+        }
+    }
+}
