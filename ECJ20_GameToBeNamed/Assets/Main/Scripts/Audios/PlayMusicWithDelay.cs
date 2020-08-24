37bc02ad7dbbc5ff915d52842b73e916b1e374dd
FrancescoS <francescoscarl93@gmail.com>
2020-08-24 17:22:57 +0200

    Main scene menu with music now

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Audios/PlayMusicWithDelay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayMusicWithDelay : MonoBehaviour
+{
+    public SimpleFMODAudioSource myAudioSource;
+    public float timeToWait = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartMusicWithDelay()); 
+    }
+
+
+    IEnumerator StartMusicWithDelay()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        myAudioSource.Play();
+    }
+}
