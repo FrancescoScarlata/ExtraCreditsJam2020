c133dcdd2344ab8d0576b3f01610aa7905a0d760
FrancescoS <francescoscarl93@gmail.com>
2020-08-29 11:19:50 +0200

    Changes to make the music hearable in webgl
    - i built in fmod "for all platforms"
    - added a starting scene "loading" to load the banks
    - added the script to wait until the banks are loaded
    - Now the webgl build has the audio working on the Mozilla firefox browser

---
+++ ECJ20_GameToBeNamed/Assets/Main/Scripts/Audios/FMODBanksLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FMODBanksLoader : MonoBehaviour
+{
+    public string[] bankNames;
+
+    public TextMeshProUGUI loadingText;
+
+    protected WaitForSeconds wait = new WaitForSeconds(0.5f);
+
+    private void Awake()
+    {
+        StartCoroutine(WaitToLoadForBanks());
+        
+    }
+
+
+    IEnumerator WaitToLoadForBanks()
+    {
+        yield return null;
+        float timePassed = 0;
+        int timeBy3;
+        yield return wait;
+
+        while (!FMODUnity.RuntimeManager.HasBanksLoaded)
+        {
+            //Debug.Log($"IsInitialized: {FMODUnity.RuntimeManager.IsInitialized}");
+            yield return wait;
+            if (loadingText)
+            {
+                timePassed += 1f;
+                timeBy3 = (int)timePassed % 4;
+                loadingText.text = "Loading";
+
+                for (int i = 0; i < timeBy3; i++)
+                {
+                    loadingText.text += ".";
+                }    
+            }
+           
+        }
+        SceneManager.LoadScene("00_MainMenu");
+    }
+
+}
